d2c06eb2211d98294772eadf68a52b8ea4ba5e8e
tsak <1022521+tsak@users.noreply.github.com>
2022-02-17 10:58:42 +0000

    Make sure relative links work for nested endpoints

--- create.bash
+++ create.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 # Adds a new endpoint and model in the given directory based on the template
 
 if [[ -z "$1" ]] || [[ -z "$2" ]]; then
@@ -12,7 +14,7 @@ NAME=${name^} # Uppercase first letter
 dir=$2
 
 if [[ ! -d "$dir" ]]; then
-    mkdir "$dir"
+    mkdir -p "$dir"
 fi
 
 cp -nv ./.template/path.yaml "$dir/"
@@ -21,6 +23,9 @@ cp -nv ./.template/model.yaml "$dir/"
 cp -nv ./.template/400.yaml "$dir/"
 cp -nv ./.template/404.yaml "$dir/"
 
+# Get relative path prefix for relative links from created dir to root
+path_prefix="$(realpath . --relative-to "$dir")/"
+
 (
     cd "$dir" || exit 1
 
@@ -28,6 +33,7 @@ cp -nv ./.template/404.yaml "$dir/"
     for template in ./path.yaml ./path-by-id.yaml ./model.yaml ./400.yaml ./404.yaml ; do
         sed -i "s/_name_/$name/g" "$template"
         sed -i "s/_Name_/$NAME/g" "$template"
+        sed -i "s#\.\./#${path_prefix}#g" "$template"
     done
 )
 
